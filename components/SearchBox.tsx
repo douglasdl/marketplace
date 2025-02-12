41926b48e41939d0cde4cb8c72133678c7824865
Douglas Dias Leal <douglas_san@hotmail.com>
2025-02-13 01:25:24 +0900

    feat: add SearchBox component

---
+++ components/SearchBox.tsx
@@ -0,0 +1,28 @@
+import { Text, TextInput, TextInputProps, View } from "react-native"
+import { Icon } from "./Icon"
+import { Button } from "./Button"
+
+export interface SearchBoxProps extends TextInputProps {
+  
+}
+
+export function SearchBox({ ...rest }: SearchBoxProps) {
+  return (
+    <View className="w-full flex-row gap-4 items-end">
+      <View className="flex-row items-center border-b border-b-gray-200 flex-1 py-3 px-0.5 gap-2">
+        <Icon name="Search01Icon" size={24} color="#949494" />
+        <TextInput 
+          placeholder="Pesquisar" 
+          className="text-gray-200 text-base font-normal"
+          {...rest} 
+        />
+      </View>
+      <Button
+        title=""
+        variation="outline"
+        iconLeft="FilterVerticalIcon"
+        onPress={() => {}}
+      />
+    </View>
+  )
+}
